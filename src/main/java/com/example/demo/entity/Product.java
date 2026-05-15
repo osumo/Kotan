66620dc8b0e48312d3672e0129545cc76ec97f9e
kintaro~ <hbakumeayato@gmail.com~>
2026-05-15 10:39:32 +0900

    mapperパッケージを作って、ProductMapperのjava.xmlを作りました

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,5 +1,35 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Product {
+	private Long id;
+	private String name;
+	private int price;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
 }
